eb2d88a6a708c502709d324c026e2f9eb99e3a38
luisjg04 <luisjg04@ucm.es>
2025-03-10 20:48:30 +0100

    añadido certificado

--- config/database.js
+++ config/database.js
@@ -1,14 +1,20 @@
 // config/database.js
 const mysql = require('mysql');
 const util = require('util');
+const fs = require('fs');
+const path = require('path');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, 'certificates/DigiCertGlobalRootCA.crt.pem'))    // Certificado de la autoridad certificadora (CA)
+    }
 });
+
 
 pool.query = util.promisify(pool.query);
 
