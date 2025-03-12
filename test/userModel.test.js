ee4a1b565ce8b0aba5a994b148bab41a5c1641f6
Paula211 <paulalsolla@gmail.com>
2025-03-12 21:58:17 +0100

    añadidos algunos tests

--- test/userModel.test.js
+++ test/userModel.test.js
@@ -20,5 +20,52 @@ describe("Registro usuario", () => {
     assert.doesNotReject(User.registro(usuario));
   });
 
+  it("No debe registrar un usuario con contraseña menor a 8 caracteres", () => {
+    const usuario = {
+      username: "Paula",
+      password: "1234567",
+    };
+
+    assert.rejects(User.registro(usuario));
+  });
+
+  it("No debe registrar un usuario con contraseña mayor a 20 caracteres", () => {
+    const usuario = {
+      username: "Paulaaa",
+      password: "123456789012345678901",
+    };
+
+    assert.rejects(User.registro(usuario));
+  });
+
+  it("No debe registrar un usuario con contraseña vacía", () => {
+    const usuario = {
+      username: "QWERT",
+      password: "",
+    };
+
+    assert.rejects(User.registro(usuario));
+  });
+
+  it("No debe registrar un usuario con nombre de usuario vacío", () => {
+    const usuario = {
+      username: "",
+      password: "12345678",
+    };
+
+    assert.rejects(User.registro(usuario));
+  });
+
+// LO SUYO SERÍA HACER EL RESTO DE COMPROBACIONES DE LA CONTRASEÑA CUANDO DESCOMENTEMOS EL HECHO DE QUE SE TIENE QUE AÑADIR
+// MAYÚSCULAS, MINÚSCULAS, NÚMEROS Y CARACTERES ESPECIALES
+
+  it("No debe registrar un usuario ya existente", () => {
+    const usuario = {
+      username: "Luis",
+      password: "234567654",
+    };
+
+    assert.rejects(User.registro(usuario));
+  });
 
 });
