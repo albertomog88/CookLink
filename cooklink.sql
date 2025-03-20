0308c79f646bbfad9555dabbb6c0aa49b6971ebe
luisjg04 <luisjg04@ucm.es>
2025-03-20 17:47:13 +0100

    simulacion sesion de usuario

--- cooklink.sql
+++ cooklink.sql
@@ -9,7 +9,6 @@ CREATE TABLE usuarios (
   password varchar(255) NOT NULL CHECK (TRIM(password) != "" AND password NOT LIKE "% %")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
 CREATE TABLE recetas (
   id int AUTO_INCREMENT PRIMARY KEY,
   nombre varchar(255) NOT NULL UNIQUE,
